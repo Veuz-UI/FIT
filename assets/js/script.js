51c7eb35133923de1f2fd045b992975a4a6df084
Bishru125 <bisharu@veuz.in>
2025-01-20 12:24:12 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) { 
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+
+// fade animation
+
+
+
+
+
+
+
+
+
+
