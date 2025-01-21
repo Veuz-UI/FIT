23aa654bb126b2b00614199c44881ade32353e61
Bishru125 <bisharu@veuz.in>
2025-01-21 13:16:50 +0300

    latest

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,27 @@ window.addEventListener('scroll', () => {
 });
 
 
-// fade animation
+// to tob Button
 
 
 
+   // Show the button when the user scrolls down
+   window.onscroll = function () {
+    var toTopBtn = document.getElementById('toTopBtn');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      toTopBtn.style.display = 'block';
+    } else {
+      toTopBtn.style.display = 'none';
+    }
+  };
 
+  // Smooth scroll to top when the button is clicked
+  document.getElementById('toTopBtn').onclick = function () {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
 
 
 
