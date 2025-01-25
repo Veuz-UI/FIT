dbd1ff8b3256ea29f622d8f1247d31e7824b67f5
Bishru125 <bisharu@veuz.in>
2025-01-25 16:52:10 +0300

    latets

--- assets/js/script.js
+++ assets/js/script.js
@@ -477,7 +477,33 @@ $(document).ready(function () {
 
 
 
+const buttons = document.querySelectorAll('.button');
 
+buttons.forEach(button => {
+  button.addEventListener('mousemove', function(evt) {
+    const movX = evt.clientX - this.getBoundingClientRect().x;
+    const movY = evt.clientY - this.getBoundingClientRect().y;
+
+    gsap.to(this.querySelector(".button__spotlight"), {
+      x: movX,
+      y: movY,
+      scale: 30,
+      duration: 0.3,
+    });
+  });
+
+  button.addEventListener('mouseleave', function(evt) {
+    const movX = evt.clientX - this.getBoundingClientRect().x;
+    const movY = evt.clientY - this.getBoundingClientRect().y;
+
+    gsap.to(this.querySelector(".button__spotlight"), {
+      x: movX,
+      y: movY,
+      scale: 0,
+      duration: 0.3,
+    });
+  });
+});
 
 
 
